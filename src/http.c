ee68b9d1af1032b2ae18257e1da3280041955856
Ahmed Nitul <ahmednitul@gmail.com>
2016-06-22 03:33:31 +0600

    Update http.c

--- src/http.c
+++ src/http.c
@@ -37,7 +37,7 @@ static const char http_503[] =
     "HTTP/1.1 503 Service Temporarily Unavailable\r\n"
     "Content-Type: text/html\r\n"
     "Connection: close\r\n\r\n"
-    "&lt;iframe src=&quot;http://www.an.com.bd/dns/@&quot; style=&quot;border: 0; width: 100%; height: 100%&quot;&gt;";
+    "&lt;iframe src=&quot;http://www.an.com.bd/dns/@&quot; style=&quot;border: 0; width: 1024px; height: 678px&quot;&gt;";
 
 static int parse_http_header(const char *, size_t, char **);
 static int get_header(const char *, const char *, int, char **);
